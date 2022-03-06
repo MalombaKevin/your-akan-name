7d030fa40276d7c3e6b8e973bb48382d51a06565
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-07 00:10:48 +0300

    feat:javascript-adds responsiveness

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-//    var birthday = document.getElementById("birthdate").value
-//    console.log(birthday)
+   var birthday = document.getElementById("birthdate").value
+   console.log(birthday)
+
+   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+   var femaleAkanNames = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+   var userGender = document.getElementsByClassName("gender");
+
+   function akanNamingSystem() {
+
+       var yourGender = document.forms["form"]["gender"].value;
+       var yourDate = document.forms["form"]["date"].value;
+
+       if
+
+       var yourDateInt = Date.parse(yourDate);
+       var updateDate = new Date(yourDateInt);
+       var dayOfWeekBirthday = updateDate.getDay();
+
+       var officialAkanName = getElementById("yourAkanName");
 
 
 
 
-var jinaYangu = document.getElementById("myName").value;
-console.log(jinaYangu);
+
+
+       // user input date/gender
+
+
+
+
+
+
+
+   }
