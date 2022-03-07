8069785d08b0ee2a2d162450da4402c6df4d641e
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-07 23:25:33 +0300

    Javascrit: adds responsiveness, get data from form, analyze and output

--- js/script.js
+++ js/script.js
@@ -1,36 +1,27 @@
-   var birthday = document.getElementById("birthdate").value
-   console.log(birthday)
+ var buttonValue = document.getElementById("sub-button");
+ var resultName = document.getElementById("yourAkanName");
 
-   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-   var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-   var femaleAkanNames = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+ buttonValue.addEventListener("click", (event) => {
+     event.preventDefault();
+     var genderValue = document.form.gender.value;
+     var birthDay = document.getElementById("birthdate").value;
+     var userBirthday = new Date(birthDay);
+     var userBirthdayDay = userBirthday.getDay();
+     var dateToday = new Date();
+     var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+     var femaleAkanNames = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-   var userGender = document.getElementsByClassName("gender");
-
-   function akanNamingSystem() {
-
-       var yourGender = document.forms["form"]["gender"].value;
-       var yourDate = document.forms["form"]["date"].value;
-
-       if
-
-       var yourDateInt = Date.parse(yourDate);
-       var updateDate = new Date(yourDateInt);
-       var dayOfWeekBirthday = updateDate.getDay();
-
-       var officialAkanName = getElementById("yourAkanName");
-
-
-
-
-
-
-       // user input date/gender
-
-
-
-
-
-
-
-   }
+     if (birthDay === "") {
+         alert("Please Enter Birthday!");
+     } else if (genderValue === "") {
+         alert("Please Select Gender!");
+     } else if (userBirthday > dateToday) {
+         alert("Enter A Valid Date");
+     } else {
+         if (genderValue === "male") {
+             resultName.innerHTML = `Your Akan Name is: ${maleAkanNames[userBirthdayDay]}`
+         } else {
+             resultName.innerHTML = `Your Akan Name is: ${femaleAkanNames[userBirthdayDay]}`
+         }
+     }
+ })
